6dd6d99b24611dc805c85b5217408d862736db8d
Vinohith <vinohith1998@gmail.com>
2019-02-26 18:58:35 +0530

    Add runsim.m

--- Aerial_Robotics/Week_3/assignment2_code/runsim.m
+++ Aerial_Robotics/Week_3/assignment2_code/runsim.m
@@ -8,6 +8,6 @@ controlhandle = @controller;
 
 % Choose which trajectory you want to test with
 trajhandle = @traj_line;
-% trajhandle = @traj_sine;
+%trajhandle = @traj_sine;
 
 [t, state] = simulation_2d(controlhandle, trajhandle);
