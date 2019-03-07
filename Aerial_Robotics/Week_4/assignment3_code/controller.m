7fa66f409bf1a14c8c5750d58a11dd45d6642ded
Vinohith <vinohith1998@gmail.com>
2019-03-07 13:50:31 +0530

    Update file with solution code

--- Aerial_Robotics/Week_4/assignment3_code/controller.m
+++ Aerial_Robotics/Week_4/assignment3_code/controller.m
@@ -24,6 +24,38 @@ F = 0;
 % Moment
 M = zeros(3,1);
 
+Kpphi = 150;
+Kdpsi = 5;
+
+Kptheta = 150;
+Kdtheta = 5;
+
+Kppsi = 150;
+Kdpsi = 5;
+
+Kp1 = 30;
+Kd1 = 2.4;
+
+Kp2 = 30;
+Kd2 = 3.2;
+
+Kp3 = 800;
+Kd3 = 50;
+
+des_state_acc_x = (des_state.acc(1)) + Kd1 * (des_state.vel(1) - state.vel(1)) + Kp1 * (des_state.pos(1) - state.pos(1));
+des_state_acc_y = (des_state.acc(2)) + Kd2 * (des_state.vel(2) - state.vel(2)) + Kp2 * (des_state.pos(2) - state.pos(2));
+
+%Thrust
+F = params.mass * (params.gravity + (des_state.acc(3)) + Kd3 * (des_state.vel(3) - state.vel(3)) + Kp3 * (des_state.pos(3) - state.pos(3)));
+
+des_state_phi = ((des_state_acc_x*sin(des_state.yaw)) - (des_state_acc_y*cos(des_state.yaw)))/params.gravity;
+des_state_theta = ((des_state_acc_x*cos(des_state.yaw)) + (des_state_acc_y*sin(des_state.yaw)))/params.gravity;
+
+% Moment
+M(1) = Kpphi*(des_state_phi - state.rot(1)) + Kdpsi*(0 - state.omega(1));
+M(2) = Kptheta*(des_state_theta - state.rot(2)) + Kdtheta*(0 - state.omega(2));
+M(3) = Kppsi*(des_state.yaw - state.rot(3)) + Kdpsi*(des_state.yawdot - state.omega(3));
+
 % =================== Your code ends here ===================
 
 end
